81ce7a77115770954aa6f52edb00860e61d3d306
Mary Ella Hill <hillmaryella@gmail.com>
2019-12-17 13:10:32 -0500

    Debugging update route to drug table

--- models/medModel.js
+++ models/medModel.js
@@ -29,7 +29,7 @@ class MedList {
     try {
       const user_id = id;
       const response = db.none(
-        `INSERT INTO medlist_id${id} (classname, drugname, strength, quantity, frequency, time, comments, formulation, category)
+        `INSERT INTO medlist_id${id} (classname, drugname, strength, quantity, frequency, time, comments, formulation, updateroute)
                                         VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9);`,
         [
           this.classname,
